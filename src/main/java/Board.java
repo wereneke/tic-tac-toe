2f927c2fcd328ab3b7a96a3a08d40da97cb91296
wereneke <weronikad752@gmail.com>
2018-04-26 16:55:59 +0200

    board has putSign responsibility

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,11 +1,28 @@
 public class Board {
 
     int size;
+    int capacity;
     char[][] board;
 
     public Board(int size) {
         this.size = size;
+        this.capacity = size*size;
         this.board = new char[size][size];
     }
 
+    public boolean putSign(int row, int col, char sign) {
+
+        if (board[row][col]=='\u0000') {
+            board[row][col] = sign;
+            capacity--;
+            return true;
+        }
+        System.out.println("the square is occupied, try again");
+        return false;
+    }
+
+    public boolean isGamePossible() {
+        System.out.println("lack of space");
+        return capacity>0;
+    }
 }
